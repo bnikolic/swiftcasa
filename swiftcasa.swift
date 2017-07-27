c54939caa3223f04865347fccc1466f5035a2fef
Bojan Nikolic <b.nikolic@mrao.cam.ac.uk>
2017-07-27 14:13:03 +0100

    Add ft and initial gaincal task

--- swiftcasa.swift
+++ swiftcasa.swift
@@ -29,9 +29,45 @@ app (file o) cpr(file i)
   // vis has to be filled before calling the CASA task. STC does not seem
   // to pick this up
   wait(vis) {
-    ovis=vis;    
+    ovis=vis;
     python_persist("import casa; casa.flagdata('%s', flagbackup=True, mode='%s', antenna='%s', spw='%s', autocorr=bool(%b)); " %
 		   (filename(ovis), mode, antenna, spw, autocorr));
+  }
+}
+
+(file ovis) casa_ft(file vis, file complist, boolean usescratch=true)
+{
+  wait(vis) {
+    ovis=vis;
+    python_persist("import casa; casa.ft('%s', complist='%s', usescratch=bool(%b));" %
+		   (filename(ovis), filename(complist), usescratch));
+  }
+}
+
+/* Basic gaincal,
+   no support yet for caltables
+*/
+(file ocal) casa_gaincal(file vis, file gaintable[],
+			 string gaintype="", string solint="",
+			 string refant="", float minsnr=1.0,
+			 string spw="" )
+{
+  ocal=noop();
+  wait(vis) {
+    wait(gaintable) {
+    python_persist("""
+import os; os.remove('%s');
+import casa;
+casa.gaincal('%s', caltable='%s',
+             gaintype='%s', solint='%s', refant='%s',
+             minsnr=%f, spw='%s');
+""" %
+		   (filename(ocal),
+		    filename(vis), filename(ocal),
+		    gaintype, solint, refant,
+		    minsnr, spw
+		    ));
+    }
   }
 }
 
@@ -43,9 +79,12 @@ app (file o) cpr(file i)
   omodel=noop();
   python_persist("""
 f='%s';
-import os;
+import os; import shutil;
 if(os.path.exists(f)):
-   os.remove(f)
+   if (os.path.isdir(f)):
+      shutil.rmtree(f)
+   else:
+      os.remove(f)
 import casac;
 cl=casac.casac.componentlist();
 cl.addcomponent(flux=%f,
